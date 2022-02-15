81bbc0d14e501d36ebea1476c3cb35c5f8481804
nikolai.smelov <yarosmilk@gmail.com>
2022-02-15 20:40:03 +0300

    -fixed some bugs

--- src/main/java/Premises.java
+++ src/main/java/Premises.java
@@ -47,35 +47,120 @@ public abstract class Premises {
         return avgRating;
     }
 
+    private void setDescription() {
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Введите название новой позиции: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() !=0) {
+                this.description = input;
+                flag = false;
+            }
+            else System.out.println("Некорректное название, введите значение ещё раз");
+        }
+    }
+
+    private void setSubwayStation() {
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Введите станцию метро: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() !=0) {
+                this.subwayStation = input;
+                flag = false;
+            } else System.out.println("Некорректное название, введите значение ещё раз");
+        }
+    }
+
+    private void setAddress() {
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Введите адрес: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() != 0) {
+                this.address = input;
+                flag = false;
+            } else System.out.println("Некорректное название, введите значение ещё раз");
+        }
+    }
+
+    private void setArea() {
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Введите площадь: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() !=0){
+                try{
+                    double possibleArea = Double.parseDouble(input);
+                    if (possibleArea>0){
+                        this.area = possibleArea;
+                        flag = false;
+                    }
+                    else System.out.println("Некорректная величина площади помещения, введите значение ещё раз");
+                }catch (Exception ex){
+                    System.out.println("Некорректная величина площади помещения, введите значение ещё раз");
+                }
+            }
+        }
+    }
+
+    private void setPrice() {
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Введите цену: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() !=0){
+                try{
+                    double possiblePrice = Double.parseDouble(input);
+                    if (possiblePrice>0){
+                        this.area = possiblePrice;
+                        flag = false;
+                    }
+                    else System.out.println("Некорректная цена помещения, введите значение ещё раз");
+                }catch (Exception ex){
+                    System.out.println("Некорректная цена помещения, введите значение ещё раз");
+                }
+            }
+        }
+    }
+
+    private void setAvgRating() {
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Введите среднюю оценку: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.length() !=0){
+                try{
+                    double possibleAvgRating = Double.parseDouble(input);
+                    if (possibleAvgRating>0){
+                        this.avgRating = possibleAvgRating;
+                        flag = false;
+                    }
+                    else System.out.println("Некорректная оценка, введите значение ещё раз");
+                }catch (Exception ex){
+                    System.out.println("Некорректная оценка, введите значение ещё раз");
+                }
+            }
+        }
+    }
+
 
     public Premises() {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-
-        System.out.print("Введите название новой позиции: ");
-        input = scanner.nextLine();
-        if(input.length() >1) this.description = input;
-
-        System.out.print("Введите станцию метро: ");
-        input = scanner.nextLine();
-        if(input.length() >1) this.subwayStation = input;
-
-        System.out.print("Введите адрес: ");
-        input = scanner.nextLine();
-        if(input.length() >1) this.address = input;
-
-        System.out.print("Введите площадь: ");
-        input = scanner.nextLine();
-        if(input.length() >1) this.area = Double.parseDouble(input);
-
-        System.out.print("Введите цену: ");
-        input = scanner.nextLine();
-        if(input.length() >1) this.price = Double.parseDouble(input);
-
-        System.out.print("Введите среднуюю оценку: ");
-        input = scanner.nextLine();
-        if(input.length() !=0) this.avgRating = Double.parseDouble(input);
+        setDescription();
+        setSubwayStation();
+        setAddress();
+        setArea();
+        setPrice();
+        setAvgRating();
     }
+
+
 
     @Override
     public String toString() {
@@ -87,6 +172,4 @@ public abstract class Premises {
                 ", цена=" + String.format("%,.2f руб.", price) +
                 ", средняя оценка=" + avgRating;
     }
-
-
 }
